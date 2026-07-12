e00e043a7aac751933e65c69b192b8e4d1cf029c
Arnav-Builds <arnav.l2209@gmail.com>
2026-07-12 18:25:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,19 @@
-var CACHE = 'lifeos-v4';
-var PAGES = [
-  '/life-os/',
-  '/life-os/index.html',
-  '/life-os/Study-tracker/index.html',
-  '/life-os/Journal-tracker/index.html',
-  '/life-os/JEE-tracker/index.html',
-  '/life-os/Habit-Tracker/index.html',
-  '/life-os/Analytics/index.html',
-  '/life-os/Pomodoro/index.html'
-];
-
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      return cache.addAll(PAGES);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keys) {
       return Promise.all(
-        keys.filter(function(key) {
-          return key !== CACHE;
-        }).map(function(key) {
+        keys.map(function(key) {
           return caches.delete(key);
         })
       );
     })
   );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request));
 });
